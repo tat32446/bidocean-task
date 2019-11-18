bc9261ce98a379b864c99e302df9e75eacc4c17c
Ram Dahal <ram.lall.dahal@ericsson.com>
2019-11-18 12:29:55 +0530

    sample html page for StaleElementReferenceException testing

--- sendkey.py
+++ sendkey.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 
+
 import pandas as pd
 
 Data=[]
@@ -13,7 +14,6 @@ Data=[]
 driver = webdriver.Chrome(executable_path='chromedriver')
 driver.get('https://www.google.com/')
 timeout = 30
-
 
 element = driver.find_element(By.XPATH,'//INPUT[@class="gLFyf gsfi"]')
 element.send_keys('testing sendkeys function')
